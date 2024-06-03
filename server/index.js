2e50777fc1cb6962076ddeaded920ca63ab05301
Rushikeshavachat <119668833+Rushikeshavachat@users.noreply.github.com>
2024-06-03 14:58:03 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(process.env.PORT, () => {
-  console.log('Server is running on port 3000!');
+  console.log('Server is running on port 3001!');
 });
 
 // app.use('/api/user', userRoutes);
